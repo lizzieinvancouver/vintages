9283f93e3cf7e986bf21cf4fa5de5a5cabb6b748
prautio <58274301+prautio@users.noreply.github.com>
2021-01-21 16:53:48 -0800

    Add files via upload
    updating script

--- data/napa_climate/Vintage_ModelingR.R
+++ data/napa_climate/Vintage_ModelingR.R
@@ -9,84 +9,89 @@ library(dplyr)
 library(lme4)
 
 #Reading in csv files
-mydat <- read.csv("/Users/phoebeautio/Desktop/Vintage Research/NapaComplete.csv", header=TRUE, na.strings=c(""," ","NA"))
+mydat <- read.csv("/Users/phoebeautio/Desktop/Vintage Research/TablesForModels/NapaComplete_phen.csv", header=TRUE, na.strings=c(""," ","NA"))
 head(mydat)
 
 ## Geoff's file path
 ## mydat <- read.csv("TablesForModels/NapaComplete_phen.csv",header = TRUE)
 
-
 stages <- sort(unique(mydat$phen_stage))
 varieties <- unique(mydat$Variety)
+
+#creating columns for each event
+mydat <- pivot_wider(mydat, names_from = phen_stage, values_from = c(st_helena_prcp, st_hosp_prcp, st_helena_gdd, st_hosp_gdd,
+                                                                    gdd_avg_phen, prcp_avg_phen))
+
+mydat <- mydat[,c(1,2,3,4,5,6,10,11,9,13,14,12,16,17,15,19,20,18,22,23,21,25,26,24,7,8)] #reorganize columns
+
 ## Temperature plots
 par(mfrow = c(length(stages), length(varieties)))
 for(i in 1:length(stages)){
-    for(j in 1:length(varieties)){
-        temp <- subset(mydat, mydat$phen_stage == stages[i] & mydat$Variety == varieties[j])
-        print(temp)
-        plot(R1_WS ~ gdd_avg_phen, data = temp, main = varieties[j])
-    }
+  for(j in 1:length(varieties)){
+    temp <- subset(mydat, mydat$phen_stage == stages[i] & mydat$Variety == varieties[j])
+    print(temp)
+    plot(R1_WS ~ gdd_avg_phen, data = temp, main = varieties[j])
+  }
 }
 
 ## Precipitation plots
 par(mfrow = c(length(stages), length(varieties)))
 for(i in 1:length(stages)){
-    for(j in 1:length(varieties)){
-        temp <- subset(mydat, mydat$phen_stage == stages[i] & mydat$Variety == varieties[j])
-        print(temp)
-        plot(R1_WS ~ prcp_avg_phen, data = temp, main = varieties[j])
-    }
+  for(j in 1:length(varieties)){
+    temp <- subset(mydat, mydat$phen_stage == stages[i] & mydat$Variety == varieties[j])
+    print(temp)
+    plot(R1_WS ~ prcp_avg_phen, data = temp, main = varieties[j])
+  }
 }
 
 ## Linear models
 
 
+#Pivoting Table 
+
 
 
 ######### OLD CODE
 
-mydat <- mydat[,c(1,10,11,12,13,2,3,4,6,7,8,5,9,14)] #reorganize columns
-
 
 #Rescaling precipitation (mm to cm)
-  
+
 #Modeling with lme4
-  
+
 
 #Fittedmodeling - "ranking = intercept + gdd * effect of gdd"
-  
-  "ranking = 73.97 + gdd * 0.0128, kinda means, ranking = 73.97 + gdd * 0"
-  
-  for(i in 1:length(varieties)){
-    temp <- subset(mydat, Variety == varieties[i])
-    mod.temp <- lm(R1_WS ~ gdd_avg * prcp_avg, data = temp) #complex, 4 things, prcp/gdd/effect of interaction/rank
-    print(varieties[i]) 
-    print(summary(mod.temp))}
-  
-  for(i in 1:length(varieties)){
-    temp <- subset(mydat, Variety == varieties[i])
-    mod.temp <- lm(R1_WS ~ gdd_avg + prcp_avg, data = temp) #simpler is better
-    print(varieties[i])
-    print(summary(mod.temp))}
-  
+
+"ranking = 73.97 + gdd * 0.0128, kinda means, ranking = 73.97 + gdd * 0"
+
+for(i in 1:length(varieties)){
+  temp <- subset(mydat, Variety == varieties[i])
+  mod.temp <- lm(R1_WS ~ gdd_avg * prcp_avg, data = temp) #complex, 4 things, prcp/gdd/effect of interaction/rank
+  print(varieties[i]) 
+  print(summary(mod.temp))}
+
+for(i in 1:length(varieties)){
+  temp <- subset(mydat, Variety == varieties[i])
+  mod.temp <- lm(R1_WS ~ gdd_avg + prcp_avg, data = temp) #simpler is better
+  print(varieties[i])
+  print(summary(mod.temp))}
+
 chard_fittedmodel <- lm(Chardonnay_Table$R1_WS ~ Chardonnay_Table$gdd_avg * Chardonnay_Table$prcp_avg)
 summary(chard_fittedmodel)  
 
 #Subset by Variety 
-  #variety
-  Cabernet <- subset(mydat, mydat$Variety=="Cabernet")
-  Chardonnay <- subset(mydat, mydat$Variety=="Chardonnay")
-  Rhone <- subset(mydat, mydat$Variety=="Rhone")
-  Merlot <- subset(mydat, mydat$Variety=="Merlot")
-  Zinfandel <- subset(mydat, mydat$Variety=="Zinfandel")
-  
+#variety
+Cabernet <- subset(mydat, mydat$Variety=="Cabernet")
+Chardonnay <- subset(mydat, mydat$Variety=="Chardonnay")
+Rhone <- subset(mydat, mydat$Variety=="Rhone")
+Merlot <- subset(mydat, mydat$Variety=="Merlot")
+Zinfandel <- subset(mydat, mydat$Variety=="Zinfandel")
+
 #Histograms
-  varieties <- unique(mydat$Variety)
-  par(mfrow = c(2, 3))
-  
-  for(i in 1:length(varieties)){
-    temp <- subset(mydat, Variety == varieties[i])
-    hist(temp$gdd_avg, main = varieties[i])}
-  
+varieties <- unique(mydat$Variety)
+par(mfrow = c(2, 3))
+
+for(i in 1:length(varieties)){
+  temp <- subset(mydat, Variety == varieties[i])
+  hist(temp$gdd_avg, main = varieties[i])}
+
 plot()
-  
