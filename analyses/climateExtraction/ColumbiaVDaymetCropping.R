95b4e0e5067f925f4463d46e4daaf809d6808007
FaithJones <faithann.jones2@gmail.com>
2021-10-14 14:54:02 -0700

    changing comumbia code to run in 3 seperate data type loops to try not to crash

--- analyses/climateExtraction/ColumbiaVDaymetCropping.R
+++ analyses/climateExtraction/ColumbiaVDaymetCropping.R
@@ -10,7 +10,16 @@ library(raster) # work with singel layer raster maps
 library(sf)# work with shapefiles (Napa shape)
 library(parallel) # for working with multiple cores
 
+#Make a function to do the cleaning
+projectAndCropList <- function(inputListNames){
+	#a function that reads in a raster, converts it to the right projection, trims to a shapefile, and saves a file
 
+		inputStack <- stack(inputListNames)
+		inputStack2 <- projectRaster(inputStack, crs = "+proj=longlat +ellps=WGS84 +no_defs")
+		inputStack3 <- crop(inputStack2, will1)
+		inputStack4 <- mask(inputStack3, will1)
+		return(inputStack4)
+}
 
 #Flaggs
 midge <- TRUE
@@ -55,24 +64,20 @@ if(midge == TRUE){
 #tmax 
 rasterFilesMax24 <- daymetFiles4[grep("max",daymetFiles4)]#select rasters with teh word max in 
 rasterFilesMax4 <- rasterFilesMax24[!rasterFilesMax24 %in% grep(".aux.xml",rasterFilesMax24, value = TRUE)]#Remove a weir d looking file
-print(rasterFilesMax4)
 
 rasterFilesMax25 <- daymetFiles5[grep("max",daymetFiles5)]#select rasters with teh word max in 
 rasterFilesMax5 <- rasterFilesMax25[!rasterFilesMax25 %in% grep(".aux.xml",rasterFilesMax25, value = TRUE)]#Remove a weir d looking file
-print(rasterFilesMax5)
 
 rasterFilesMax27 <- daymetFiles7[grep("max",daymetFiles7)]#select rasters with teh word max in 
 rasterFilesMax7 <- rasterFilesMax27[!rasterFilesMax27 %in% grep(".aux.xml",rasterFilesMax27, value = TRUE)]#Remove a weir d looking file
-print(rasterFilesMax7)
 
 rasterFilesMax28 <- daymetFiles8[grep("max",daymetFiles8)]#select rasters with teh word max in 
 rasterFilesMax8 <- rasterFilesMax28[!rasterFilesMax28 %in% grep(".aux.xml",rasterFilesMax28, value = TRUE)]#Remove a weir d looking file
-print(rasterFilesMax8)
 
 rasterFilesMax29 <- daymetFiles9[grep("max",daymetFiles9)]#select rasters with teh word max in 
 rasterFilesMax9 <- rasterFilesMax29[!rasterFilesMax29 %in% grep(".aux.xml",rasterFilesMax29, value = TRUE)]#Remove a weir d looking file
 
-print(rasterFilesMax9)
+maxFiles <- list(rasterFilesMax4, rasterFilesMax5,rasterFilesMax7,rasterFilesMax8,rasterFilesMax9)
 
 
 #tmin
@@ -91,6 +96,8 @@ rasterFilesMin8 <- rasterFilesMin28[!rasterFilesMin28 %in% grep(".aux.xml",raste
 rasterFilesMin29 <- daymetFiles9[grep("min", daymetFiles9)]#select rasters with teh word min in 
 rasterFilesMin9 <- rasterFilesMin29[!rasterFilesMin29 %in% grep(".aux.xml",rasterFilesMin29, value = TRUE)]
 
+minFiles <- list(rasterFilesMin4, rasterFilesMin5,rasterFilesMin7,rasterFilesMin8,rasterFilesMin9)
+
 
 #Precipitation
 rasterFilesPrcp24 <- daymetFiles4[grep("prcp", daymetFiles4)]#select rasters with teh word max in 
@@ -108,6 +115,11 @@ rasterFilesPrcp8 <- rasterFilesPrcp28[!rasterFilesPrcp28 %in% grep(".aux.xml",ra
 rasterFilesPrcp29 <- daymetFiles9[grep("prcp", daymetFiles9)]#select rasters with teh word max in 
 rasterFilesPrcp9 <- rasterFilesPrcp29[!rasterFilesPrcp29 %in% grep(".aux.xml",rasterFilesPrcp29, value = TRUE)]
 
+prcpFiles <- list(rasterFilesPrcp4, rasterFilesPrcp5,rasterFilesPrcp7,rasterFilesPrcp8,rasterFilesPrcp9)
+
+#Make a list of all files that I can loop through
+allFiles <- list(maxFiles,minFiles,prcpFiles)
+
 #Set correct projection 
 correctProjection <- "+proj=longlat +ellps=WGS84 +no_defs"
 
@@ -115,142 +127,109 @@ correctProjection <- "+proj=longlat +ellps=WGS84 +no_defs"
 nCore <- detectCores()/2 
 
 
-#Merge the tiles together 
-#_------------------------------------
-
-	raster36ListMax <- list()
-	raster36ListMin <- list()
-	raster36ListPrecp <- list()
-
-if(Trial == TRUE){ # trial smaller version to get things working 
-
-	for(i in 1:2){
-		#i <-1
-	#tmax
-	raster4Max <- stack(rasterFilesMax4[i])
-	raster5Max <- stack(rasterFilesMax5[i])
-	raster7Max <- stack(rasterFilesMax7[i])
-	raster8Max <- stack(rasterFilesMax8[i])
-	raster9Max <- stack(rasterFilesMax9[i])
-	raster36Max <- merge(raster4Max, raster5Max, raster7Max,raster8Max,raster9Max)
-	names(raster36Max) <- names(raster4Max)
-	raster36ListMax[[i]] <- raster36Max
-
-	#Tmin
-	raster4Min <- stack(rasterFilesMin4[i])
-	raster5Min <- stack(rasterFilesMin5[i])
-	raster7Min <- stack(rasterFilesMin7[i])
-	raster8Min <- stack(rasterFilesMin8[i])
-	raster9Min <- stack(rasterFilesMin9[i])
-	raster36Min <- merge(raster4Min, raster5Min, raster7Min,raster8Min,raster9Min)
-	names(raster36Min) <- names(raster4Min)
-	raster36ListMin[[i]] <- raster36Min
-
-
-	#Precipitation
-	raster4Prec <- stack(rasterFilesPrcp4[i])
-	raster5Prec <- stack(rasterFilesPrcp5[i])
-	raster7Prec <- stack(rasterFilesPrcp7[i])
-	raster8Prec <- stack(rasterFilesPrcp8[i])
-	raster9Prec <- stack(rasterFilesPrcp9[i])
-	raster36Prec <- merge(raster4Prec, raster5Prec, raster7Prec,raster8Prec,raster9Prec)
-	names(raster36Prec) <- names(raster4Prec)
-	raster36ListPrecp[[i]] <- raster36Prec
-	}
-
-
-}
-
-
-if(Trial == FALSE){
-
-
-	for(i in 1:length(rasterFilesMax4)){
-				#i <-1
-	#tmax
-		raster4Max <- stack(rasterFilesMax4[i])
-	raster5Max <- stack(rasterFilesMax5[i])
-	raster7Max <- stack(rasterFilesMax7[i])
-	raster8Max <- stack(rasterFilesMax8[i])
-	raster9Max <- stack(rasterFilesMax9[i])
-	raster36Max <- merge(raster4Max, raster5Max, raster7Max,raster8Max,raster9Max)
-	names(raster36Max) <- names(raster4Max)
-	raster36ListMax[[i]] <- raster36Max
-
-	#Tmin
-	raster4Min <- stack(rasterFilesMin4[i])
-	raster5Min <- stack(rasterFilesMin5[i])
-	raster7Min <- stack(rasterFilesMin7[i])
-	raster8Min <- stack(rasterFilesMin8[i])
-	raster9Min <- stack(rasterFilesMin9[i])
-	raster36Min <- merge(raster4Min, raster5Min, raster7Min,raster8Min,raster9Min)
-	names(raster36Min) <- names(raster4Min)
-	raster36ListMin[[i]] <- raster36Min
-
-
-	#Precipitation
-	raster4Prec <- stack(rasterFilesPrcp4[i])
-	raster5Prec <- stack(rasterFilesPrcp5[i])
-	raster7Prec <- stack(rasterFilesPrcp7[i])
-	raster8Prec <- stack(rasterFilesPrcp8[i])
-	raster9Prec <- stack(rasterFilesPrcp9[i])
-	raster36Prec <- merge(raster4Prec, raster5Prec, raster7Prec,raster8Prec,raster9Prec)
-	names(raster36Prec) <- names(raster4Prec)
-	raster36ListPrecp[[i]] <- raster36Prec
-	}
-
-}
-
-
 #work up to processing multiple layers - look through each year and save seperate layers
 #------------------------------------------
 
-#Make a function to do the cleaning
-projectAndCropList <- function(inputListNames){
-	#a function that reads in a raster, converts it to the right projection, trims to a shapefile, and saves a file
+#Get mean values in a loop to save having too much in the memory
+#--------------------------------------------------------------------
+#make a vector of file names 
+filenames <- c( "tmax/tmax_","tmin/tmin_","prcp/prcp_")
+namesLoop <- c("Max","Min", "Precp") #names used to assign a value to teh output of the loop 
 
-		inputStack <- stack(inputListNames)
-		inputStack2 <- projectRaster(inputStack, crs = "+proj=longlat +ellps=WGS84 +no_defs")
-		inputStack3 <- crop(inputStack2, will1)
-		inputStack4 <- mask(inputStack3, will1)
-		return(inputStack4)
-}
 
+#combine lists of raster file names into a new list for looping
+rasterList <- list()
+
+
+for(iData in 1:3){
 #Maximum values
-#-------------------------------------------------
-rasterMaxCrop <- lapply(raster36ListMax, FUN = projectAndCropList)#, mc.cores = nCore) # paraleliszed lapply 
+	
+	#iData <- 1
+	
+	#Merge the tiles together 
+	#_------------------------------------
 
-#Minimum values
-#----------------------------
-rasterMinCrop <- lapply(raster36ListMin, FUN = projectAndCropList)#, mc.cores = nCore) # paraleliszed lapply 
+	for(i in 1:length(rasterFilesMax4)){ # combine all the maps together
+		#i <-1
+		raster4Max <- stack(allFiles[[iData]][[1]][i])
+		raster5Max <- stack(allFiles[[iData]][[2]][i])
+		raster7Max <- stack(allFiles[[iData]][[3]][i])
+		raster8Max <- stack(allFiles[[iData]][[4]][i])
+		raster9Max <- stack(allFiles[[iData]][[5]][i])
+		raster36Max <- merge(raster4Max, raster5Max, raster7Max,raster8Max,raster9Max)
+		names(raster36Max) <- names(raster4Max)
+		rasterList[[i]] <- raster36Max
+	}
 
-#Precipitation 
-#-------------------------------
-rasterMaxPrcp <- lapply(raster36ListPrecp, FUN = projectAndCropList)#, mc.cores = nCore) # paraleliszed lapply 
+	#-------------------------------------------------
+	rasterCrop <- lapply(rasterList, FUN = projectAndCropList)#, mc.cores = nCore) # paraleliszed lapply 
+	assign(paste("rasterCrop", namesLoop[[iData]], sep = ""),rasterCrop)#give this a name I can call outside of the loop
+
+	#Write files
+	#----------------------------
+
+	if(SaveMapFiles == TRUE){
+		for(i in 1:(length(rasterFilesMax4))){
+			writeRaster(stack(rasterCrop[[i]]), filename=paste0(paste0(filenames[iData],i+1979), ".nc"), overwrite=TRUE, format="CDF") 
+		}
+	}
+
+	#Maximum values
+	#-------------------------------------------------
+
+	rasterList <- list() # Empty the list at the end of the itteration so I don't use too much memory
+}
 
 
 #Write files
 #----------------------------
 
 if(SaveMapFiles == TRUE){
-	for(i in 1:(length(rasterFilesMax4))){
+	for(i in 1:(length(rasterFilesMax))){
 		#i <- 1
-		writeRaster(stack(rasterMaxCrop[[i]]), filename=paste0(paste0( "tmax/tmax_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
-		writeRaster(stack(rasterMinCrop[[i]]), filename=paste0(paste0( "tmin/tmin_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
-		writeRaster(stack(rasterMaxPrcp[[i]]), filename=paste0(paste0( "prcp/prcp_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
+		writeRaster(stack(rasterCropMax[[i]]), filename=paste0(paste0( "sonoma/tmax/tmax_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
+		writeRaster(stack(rasterCropMin[[i]]), filename=paste0(paste0( "sonoma/tmin/tmin_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
+		writeRaster(stack(rasterCropPrecp[[i]]), filename=paste0(paste0( "sonoma/prcp/prcp_",i+1979), ".nc"), overwrite=TRUE, format="CDF") # FAITH: update your directory here
 	}
 
 }
 
 
+
 #Get mean per layer
 #--------------------------- 
+
+if(Trial == TRUE){
+
+	rasterMaxCropTrial <-  mclapply(rasterFilesMax[1:2], FUN = projectAndCropList, mc.cores = 2) # paraleliszed lapply 
+	rasterMainCropTrial <-  mclapply(rasterFilesMin[1:2], FUN = projectAndCropList, mc.cores = 2) # paraleliszed lapply 
+
+	dailyMax <- list()
+
+	for(ip in 1:length(rasterMaxCropTrial)){
+	
+
+		#get mean per cell
+
+		#get mean accross cells
+		rastersList <- list(c(rasterMaxCropTrial[[ip]], rasterMainCropTrial[[ip]]))
+		minMax <-  lapply(rastersList, stack)
+
+		dailyMaxTempNapa  <- cellStats(stack(minMax), stat='mean')	
+		maxdf <- data.frame(dailyMaxTempNapa)
+		maxdf$date <- rownames(maxdf)
+		rownames(maxdf) <- NULL
+		dailyMax[[ip]] <- maxdf
+	}
+
+	tMaxTrial <- do.call("rbind", dailyMax)
+}
 
 
 #maximum temp 
 dailyMax <- list()
 
-	for(ip in 1:length(rasterFilesMax4)){
+	for(ip in 1:length(rasterFilesMax)){
 	
 		dailyMaxTempNapa  <- cellStats(rasterMaxCrop[[ip]], stat='mean') # get mean per day per map
 		maxdf <- data.frame(dailyMaxTempNapa)
@@ -264,13 +243,13 @@ tMaxTrial <- do.call("rbind", dailyMax)
 #Mean temperature
 dailyMean <- list()
 
-	for(ip in 1:length(rasterFilesMin4)){
+	for(ip in 1:length(rasterFilesMin)){
 	
 
 		#get mean per cell
 
 		#get mean accross cells
-		rastersListMean <- list(c(rasterFilesMax4[[ip]], rasterFilesMin4[[ip]]))
+		rastersListMean <- list(c(rasterFilesMax[[ip]], rasterFilesMin[[ip]]))
 		minMax <-  lapply(rastersListMean, stack)
 
 		dailyMMTempNapa  <- cellStats(stack(minMax), stat='mean')	
@@ -285,7 +264,7 @@ tMeanTrial <- do.call("rbind", dailyMean)
 #minimum temp 
 dailyMin <- list()
 
-	for(ip in 1:length(rasterFilesMin4)){
+	for(ip in 1:length(rasterFilesMin)){
 	
 
 		dailyMinTempNapa  <- cellStats(rasterMinCrop[[ip]], stat='mean') # get mean per day per map
@@ -301,7 +280,7 @@ tMinTrial <- do.call("rbind", dailyMin)
 #precipitation 
 dailyPrecp <- list()
 
-	for(ip in 1:length(rasterFilesPrcp4)){
+	for(ip in 1:length(rasterMaxPrcp)){
 	
 		dailyPrTempNapa  <- cellStats(rasterMaxPrcp[[ip]], stat='mean') # get mean per day per map
 		prdf <- data.frame(dailyPrTempNapa)
@@ -311,6 +290,7 @@ dailyPrecp <- list()
 	}
 
 prTrial <- do.call("rbind", dailyPrecp)
+
 
 
 if(SaveCSVFiles == TRUE){
